cc997fd25b053338d772b9c8e49545a210ea8a4c
tushar <tushargoyal100@gmail.com>
2021-11-22 08:04:19 +0530

    adding authorization

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 module.exports.isLoggedIn=(req,res,next)=>{
-    console.log("REQ.USER...",req.user)
+   // console.log("REQ.USER...",req.user)
     if(!req.isAuthenticated())
-    {
+    {   
         req.flash('error','you must be signed in');
         return res.redirect('/login');
     }
