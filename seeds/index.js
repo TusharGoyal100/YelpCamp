17d3a185b079b8df9d524c0c01a61beb8d769380
tushar <tushargoyal100@gmail.com>
2021-12-07 08:07:58 +0530

    adding cluster map

--- seeds/index.js
+++ seeds/index.js
@@ -17,7 +17,7 @@ const sample=array=>array[Math.floor(Math.random()*array.length)];
 
 const seedDB=async()=>{
     await Campground.deleteMany({});
-    for(let i=0;i<50;i++)
+    for(let i=0;i<300;i++)
     {
         const random1000=Math.floor(Math.random()*1000);
         const price=Math.floor(Math.random()*20)+10;
@@ -27,6 +27,13 @@ const seedDB=async()=>{
             title:`${sample(descriptors)} ${sample(places)}`,
             description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos similique praesentium ratione dolor voluptates repellendus ipsum laboriosam amet error, aliquid culpa excepturi modi nisi laudantium sapiente. Itaque, quas hic?',
             price:price,
+            geometry:{
+                type:"Point",
+                coordinates:[
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
+            },
             image: [
                 {
                   url: 'https://res.cloudinary.com/dy1lw8otu/image/upload/v1638321757/YelpCamp/zoltz1hqqggz9he8bjkk.jpg',
